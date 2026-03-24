e54651a707bc115762512bbb56439717e8db6d0c
jumaragor <jumar.agor2018@gmail.com>
2026-03-24 12:28:19 +0800

    Delete components directory

--- components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-type CardProps = PropsWithChildren<{
-  className?: string;
-}>;
-
-export function Card({ className, children }: CardProps) {
-  return <section className={className ? `card ${className}` : "card"}>{children}</section>;
-}
